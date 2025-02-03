8b50d80cc0bd400cdf5b63ab776ba02aa7a80561
yannick radji <yannickradji@gmail.com>
2025-02-03 21:26:04 +0000

    simplify customer_orders

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -33,24 +33,25 @@ paid_orders as (
     from orders
     left join finalized_orders on orders.id = finalized_orders.order_id
     left join customers on orders.user_id = customers.id
+),
+
+final as (
+    select
+        paid_orders.*,
+
+        ROW_NUMBER() OVER (ORDER BY paid_orders.order_id) as transaction_seq,
+        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY paid_orders.order_id) as customer_sales_seq,
+
+        min(paid_orders.order_placed_at) OVER (PARTITION by customer_id ) as first_order_date,
+        CASE WHEN first_order_date = paid_orders.order_placed_at
+        THEN 'new'
+        ELSE 'return' END as nvsr,
+        first_order_date as fdos,
+        
+        sum(paid_orders.total_amount_paid) OVER (PARTITION BY paid_orders.customer_id order by paid_orders.order_id) as customer_lifetime_value
+        
+    FROM paid_orders
 )
 
-select
-    paid_orders.*,
-
-    ROW_NUMBER() OVER (ORDER BY paid_orders.order_id) as transaction_seq,
-    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY paid_orders.order_id) as customer_sales_seq,
-
-    min(paid_orders.order_placed_at) OVER (PARTITION by customer_id ) as first_order_date,
-
-    CASE WHEN first_order_date = paid_orders.order_placed_at
-    THEN 'new'
-    ELSE 'return' END as nvsr,
-    
-    sum(paid_orders.total_amount_paid) OVER (PARTITION BY paid_orders.customer_id order by paid_orders.order_id) as customer_lifetime_value,
-    
-    first_order_date as fdos
-    
-FROM paid_orders
-left join customer_orders as c USING (customer_id)
+select * from final
 ORDER BY order_id
